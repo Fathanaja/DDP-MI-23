fb4cd10081c65e2ef5863b5095c1307cd788d169
fathanaja <fathancover@gmail.com>
2023-10-01 22:05:50 +0700

    Tugas IF

---
+++ 03 Percabangan/if4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka;
+    cin >> angka;
+
+    if (angka % 2 == 1)
+    {
+        cout << "Ganjil";
+    }
+    else
+    {
+        cout << "Genap";
+    }
+}
